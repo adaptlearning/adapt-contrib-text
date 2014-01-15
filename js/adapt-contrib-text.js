588b87050983e3beedf97f4ff88672703778387e
Daryl Hedley <darylhedley@hotmail.com>
2014-01-15 09:18:27 +0000

    Fixed inview bug with text

--- js/adapt-contrib-text.js
+++ js/adapt-contrib-text.js
@@ -1,11 +1,22 @@
-define(["coreViews/componentView", "coreJS/adapt"], function(ComponentView, Adapt) {
+/*
+* adapt-contrib-text
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Daryl Hedley <darylhedley@hotmail.com>
+*/
+define(function(require) {
+
+	var ComponentView = require('coreViews/componentView');
+	var Adapt = require('coreJS/adapt');
 
     var Text = ComponentView.extend({
         
         postRender: function() {
-            console.log("rendering");
+        	this.$('.text-body').on('inview', _.bind(function(visible) {
+            	if (visible) {
+            		this.setCompletionStatus();
+            	}
+            }, this));
             this.setReadyStatus();
-            this.setCompletionStatus();
         }
         
     });
