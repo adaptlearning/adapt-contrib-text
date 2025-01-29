7d253106bdbdfa58da9de08c4928f05e33baabc9
danielghost <daniel.ghost@kineo.com>
2025-01-29 09:55:54 +0000

    Removed v1 tasks as no need to migrate. Added `setObjectPathValue` and updated tasks accordingly. Fixed an issue with `ariaRegion` not always being updated to 6.1.3.

--- migrations/v2.js
+++ migrations/v2.js
@@ -5,18 +5,27 @@ function hasKey(object, key) {
   return Object.hasOwn(object, key);
 }
 
-describe('adapt-contrib-text - v1.0.0 to v2.0.1', async () => {
-  whereFromPlugin('adapt-contrib-text - from v1.0.0 to v2.0.1', { name: 'adapt-contrib-text', version: '<2.0.1'});
+function setObjectPathValue(object, path, value, force = false) {
+  if (!object) return;
+  const paths = path.split('.');
+  const key = paths.pop();
+  const target = paths.reduce((o, p) => {
+    if (!hasKey(o, p)) o[p] = {};
+    return o?.[p];
+  }, object);
+  if (!force && hasKey(target, key)) return;
+  target[key] = value;
+}
+
+describe('adapt-contrib-text - v2.0.0 to v2.0.1', async () => {
+  whereFromPlugin('adapt-contrib-text - from v2.0.0 to v2.0.1', { name: 'adapt-contrib-text', version: '<2.0.1'});
   let course;
   whereContent('adapt-contrib-text - where missing _globals ariaRegion', async content => {
     course = content.find(({ _type }) => _type === 'course');
     return !hasKey(course?._globals?._components?._text, 'ariaRegion');
   });
   mutateContent('adapt-contrib-text - add _globals ariaRegion', async () => {
-    if (!course._globals) course._globals = {};
-    if (!course._globals._components) course._globals._components = {};
-    if (!course._globals._components._text) course._globals._components._text = {};
-    course._globals._components._text.ariaRegion = '';
+    setObjectPathValue(course, '_globals._components._text.ariaRegion', '');
     return true;
   });
   checkContent('adapt-contrib-text - check _globals ariaRegion added', async () => {
--- migrations/v6.js
+++ migrations/v6.js
@@ -5,22 +5,28 @@ function hasKey(object, key) {
   return Object.hasOwn(object, key);
 }
 
+function setObjectPathValue(object, path, value, force = false) {
+  if (!object) return;
+  const paths = path.split('.');
+  const key = paths.pop();
+  const target = paths.reduce((o, p) => {
+    if (!hasKey(o, p)) o[p] = {};
+    return o?.[p];
+  }, object);
+  if (!force && hasKey(target, key)) return;
+  target[key] = value;
+}
+
 /**
  * correct v1 schemas - legacy schema correct in v2.0.1
  */
 describe('adapt-contrib-text - v4.2.0 to v6.1.3', async () => {
   whereFromPlugin('adapt-contrib-text - from v4.2.0 to v6.1.3', { name: 'adapt-contrib-text', version: '<6.1.3'});
   let course;
-  whereContent('adapt-contrib-text - where incorrect _globals ariaRegion nesting', async content => {
+  mutateContent('adapt-contrib-text - update _globals ariaRegion', async content => {
     course = content.find(({ _type }) => _type === 'course');
-    return hasKey(course?._globals, '_text');
-  });
-  mutateContent('adapt-contrib-text - update _globals ariaRegion', async () => {
-    if (!course._globals) course._globals = {};
-    if (!course._globals._components) course._globals._components = {};
-    if (!course._globals._components._text) course._globals._components._text = {};
-    course._globals._components._text.ariaRegion = course?._globals?._text?.ariaRegion ?? '';
-    delete course._globals._text;
+    setObjectPathValue(course, '_globals._components._text.ariaRegion', '');
+    delete course?._globals?._text;
     return true;
   });
   checkContent('adapt-contrib-text - check _globals ariaRegion updated', async () => {
